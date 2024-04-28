74d4a7ed1d6b3c0518ab92eab15c7314481c7957
Chili <vasapofficial@gmail.com>
2024-04-28 16:43:20 +0200

    style: completely change the footer design and style

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -1,50 +1,272 @@
 import Image from 'next/image';
 import Logo from '@/public/assets/logoWhite.webp'
 import Link from 'next/link';
-import SocialIcons from './ui/social-icons';
-
-import phoneIcon from '@/public/assets/footer/phone.webp'
-import emailIcon from '@/public/assets/footer/email.webp'
 
 function Footer() {
-    return ( 
-      <footer className="bg-black text-white w-full from-neutral-200 to-neutral-500">
-        <div className="max-w-[80%] mx-auto py-16">
-          <Image src={Logo} alt="Logo" width={400} height={400}/>
-          <div className="flex mt-10 justify-between max-lg:flex-col max-lg:items-start max-lg:gap-10 max-lg:justify-center">
-            <div className="flex flex-col">
-              <h1 className="font-bold text-3xl mb-3">Locations</h1>
-              <Link href='/' className="linkWithLine mb-2 cursor-pointer font-extrabold hover:text-[darkRed]">Europe</Link>
-              <Link href='/' className="linkWithLine mb-2 cursor-pointer font-extrabold hover:text-[darkRed]">Asia</Link>
-              <Link href='/' className="linkWithLine mb-2 cursor-pointer font-extrabold hover:text-[darkRed]">Africa</Link>
-              <Link href='/' className="linkWithLine mb-2 cursor-pointer font-extrabold hover:text-[darkRed]">North America</Link>
-              <Link href='/' className="linkWithLine mb-2 cursor-pointer font-extrabold hover:text-[darkRed]">South America</Link>
-              <Link href='/' className="linkWithLine mb-2 cursor-pointer font-extrabold hover:text-[darkRed]">Oceania</Link>
+  return ( 
+    <footer className="h-auto w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative">
+      <div className="absolute z-10 pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)]"></div>
+      <div className="mx-auto max-w-screen-xl px-4 pb-6 pt-16 sm:px-6 lg:px-8 lg:pt-24">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          <div className='z-20'>
+            <div className="flex justify-center text-teal-600 sm:justify-start dark:text-teal-300">
+              <Image src={Logo} alt='logo icon' width={300} height={100} className='z-50'/>
             </div>
-            <div>
-              <h1 className="font-bold text-3xl mb-3">Contact</h1>
-              <div className="flex mb-3">
-                <Image src={emailIcon} alt="email icon" width={30} height={30} className="w-auto h-auto"/>
-                <p className="ml-3 mt-2">travelagency@gmail.com</p>
-              </div>
-              <div className="flex">
-                <Image src={phoneIcon} alt="phone icon" width={30} height={30} className="w-auto h-auto"/>
-                <p className="ml-3 mt-2">032131203192</p>
-              </div>
+            <p className="mt-6 max-w-md text-center leading-relaxed text-gray-500 sm:max-w-xs sm:text-left dark:text-gray-300 z-50">
+              Welcome to Odyssey Travel, your passport to unforgettable adventures! Explore our exotic destinations and embark on journeys that redefine wanders. Let Odyssey Travel be your compass in navigating the world's wonders.
+            </p>
+            <ul className="mt-8 flex justify-center gap-6 sm:justify-start md:gap-8">
+              <li>
+                <Link
+                  href="#"
+                  className="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75">
+                  <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                    <path
+                      fillRule="evenodd"
+                      d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75">
+                  <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                    <path
+                      fillRule="evenodd"
+                      d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75">
+                  <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                    <path
+                      d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+                    />
+                  </svg>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-teal-700 transition hover:text-teal-700/75 dark:text-teal-500 dark:hover:text-teal-500/75">
+                  <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4 lg:col-span-2">
+            <div className="text-center sm:text-left z-20">
+              <p className="text-lg font-medium text-gray-300">About Us</p>
+              <ul className="mt-8 space-y-4 text-sm">
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                    href="#">
+                    Company History
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                    href="#">
+                    Meet the Team
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                    href="#">
+                    Employee Handbook
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                    href="#">
+                    Careers
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <div className="flex flex-col items-center max-lg:items-start">
-              <h1 className="font-bold text-3xl mb-3">Social Media</h1>
-              <SocialIcons />
+            <div className="text-center sm:text-left z-20">
+              <p className="text-lg font-medium text-gray-300">Our Services</p>
+              <ul className="mt-8 space-y-4 text-sm">
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75 z-20"
+                    href="#">
+                    Web Development
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75 z-20"
+                    href="#">
+                    Web Design
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75 z-20"
+                    href="#">
+                    Marketing
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75 z-20"
+                    href="#">
+                    Google Ads
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="text-center sm:text-left z-20">
+              <p className="text-lg font-medium text-gray-300">Helpful Links</p>
+              <ul className="mt-8 space-y-4 text-sm">
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75 z-20"
+                    href="#">
+                    FAQs
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75 z-20" 
+                    href="#">
+                    <span className='z-20'>Support</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="group flex gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
+                    href="#">
+                    <span
+                      className="text-gray-700 transition group-hover:text-gray-200/75 dark:text-white dark:hover:text-white/75 z-20">
+                      Live Chat
+                    </span>
+                    <span className="relative flex h-2 w-2">
+                      <span
+                        className="absolute inline-flex h-full w-full animate-ping rounded-full bg-teal-400 opacity-75"
+                      ></span>
+                      <span className="relative inline-flex size-2 rounded-full bg-teal-500"></span>
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="text-center sm:text-left z-20">
+              <p className="text-lg font-medium text-gray-300">Contact Us</p>
+              <ul className="mt-8 space-y-4 text-sm">
+                <li>
+                  <Link
+                    className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
+                    href="#">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="size-5 shrink-0 text-gray-900 dark:text-white z-20"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                      />
+                    </svg>
+                    <span className="flex-1 text-gray-700 dark:text-gray-300 z-20"> john@doe.com </span>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
+                    href="#">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="size-5 shrink-0 text-gray-900 dark:text-white z-20"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                      />
+                    </svg>
+                    <span className="flex-1 text-gray-700 dark:text-gray-300 z-20">0123456789</span>
+                  </Link>
+                </li>
+                <li className="flex items-start justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-5 shrink-0 text-gray-900 dark:text-white z-20"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                  <address className="-mt-0.5 flex-1 not-italic text-gray-700 dark:text-gray-300 z-20">
+                    213 Lane, London, United Kingdom
+                  </address>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="mt-16 flex w-full items-center justify-center border-t">
-          <span className="mt-10 text-center text-base leading-6">
-          © 2023 Travel Agency. All rights reserved.
-          </span>
         </div>
+        <div className="mt-12 border-t border-gray-100 pt-6 dark:border-gray-800">
+          <div className="text-center sm:flex sm:justify-between sm:text-left">
+            <p className="text-sm text-gray-500 dark:text-gray-400 z-20">
+              <span className="block sm:inline">All rights reserved.</span>
+              <a
+                className="inline-block text-teal-600 underline transition hover:text-teal-600/75 dark:text-teal-500 dark:hover:text-teal-500/75"
+                href="#">
+                Terms & Conditions
+              </a>
+              <span>&middot;</span>
+              <a
+                className="inline-block text-teal-600 underline transition hover:text-teal-600/75 dark:text-teal-500 dark:hover:text-teal-500/75"
+                href="#">
+                Privacy Policy
+              </a>
+            </p>
+            <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0 dark:text-gray-400 z-50">
+              &copy; 2022 Odyssey Travel
+            </p>
+          </div>
         </div>
-      </footer>
-    );
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
