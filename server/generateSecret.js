f2d00ed818adbb4c6ca9cba1219207a5ca5643df
Chili <vasapofficial@gmail.com>
2024-04-30 16:33:20 +0200

    refactor: remove the auth logic from the server

--- server/generateSecret.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a random string for JWT secret
-const generateJWTSecret = () => {
-  return crypto.randomBytes(32).toString('hex');
-};
-
-console.log(generateJWTSecret());
--- server/server.js
+++ server/server.js
@@ -1,28 +1,20 @@
 const express = require('express')
-const mongoose = require('mongoose')
 require('dotenv').config()
 const cors = require('cors');
-const registerRoute = require('./routes/register');
-const loginRoute = require('./routes/login');
 
 const app = express()
 const PORT = process.env.PORT || 8000
-const MONGODB_URL = process.env.MONGODB_URL
 
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
 
-mongoose.connect(MONGODB_URL)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err))
-
 app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
-
-//routes
-app.use('/register', registerRoute);
-app.use('/login', loginRoute);
