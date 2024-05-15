4a28625f0cc3cb4fd45e5f2fe9bb5657ffb50889
Chili <vasapofficial@gmail.com>
2024-05-15 20:51:03 +0200

    feat: make top offers dynamic

--- client/src/app/pages/landing-page/TopOffers.tsx
+++ client/src/app/pages/landing-page/TopOffers.tsx
@@ -1,18 +1,77 @@
+'use client'
+import { useEffect } from 'react';
 import { BentoGrid, BentoGridItem } from '../../components/ui/bento-grid';
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios';
+
+export type TravelPlan = {
+  day: string;
+  description: string;
+};
+
+export type TopOffer = {
+  id: number;
+  city: string;
+  country: string;
+  price: number;
+  imageUrl: string;
+  dateRange: string;
+  days: number;
+  shortDescription: string;
+  description: string;
+  tripRoute: string;
+  travelPlan: TravelPlan[];
+  includedIn: string;
+}
 
 export default function BentoGridDemo() {
+  const { isError, data } = useQuery({ 
+    queryKey: ['topOffers'], 
+    queryFn: fetchOffers,
+    retry: 3,
+    retryDelay: 1000,
+  })
+
+  async function fetchOffers() {
+    try {
+      const response = await axios.get('http://localhost:8000/api/data/TopOffers');
+  
+      if (response.status !== 200) {
+        throw new Error('API request failed');
+      }
+
+      console.log(response.data)
+      return response.data as TopOffer[];
+    } catch (error) {
+      console.error('Error fetching continents:', error);
+      throw error; // Re-throw for handling in useQuery
+    }
+  }
+
+  useEffect(() => {
+    console.log(data)
+  },[]);
+
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <section className='pt-10'>
       <div className='max-w-6xl mx-auto py-12'>
         <h1 className='from-neutral-200 to-neutral-500 text-white font-semibold text-4xl'>Popular Destinations</h1>
         <BentoGrid className="max-w-6xl mx-auto py-5">
-          {items.map((item, i) => (
+          {data.map((item: TopOffer, i: number) => (
             <BentoGridItem
               key={i}
-              title={item.title}
-              description={item.description}
-              header={item.header}
-              className={`cursor-pointer ${i === 3 || i === 6 ? 'md:col-span-2' : ''}`}
+              title={item.city}
+              description={item.shortDescription}
+              header={ <Skeleton />}
+              className={`cursor-pointer ${i === 3 || i === 6 ? 'md:col-span-1' : ''}`}
             />
           ))}
         </BentoGrid>
@@ -23,41 +82,3 @@ export default function BentoGridDemo() {
 const Skeleton = () => (
   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
 );
-const items = [
-  {
-    title: 'The Dawn of Innovation',
-    description: 'Explore the birth of groundbreaking ideas and inventions.',
-    header: <Skeleton />,
-  },
-  {
-    title: 'The Digital Revolution',
-    description: 'Dive into the transformative power of technology.',
-    header: <Skeleton />,
-  },
-  {
-    title: 'The Art of Design',
-    description: 'Discover the beauty of thoughtful and functional design.',
-    header: <Skeleton />,
-  },
-  {
-    title: 'The Power of Communication',
-    description:
-      'Understand the impact of effective communication in our lives.',
-    header: <Skeleton />,
-  },
-  {
-    title: 'The Pursuit of Knowledge',
-    description: 'Join the quest for understanding and enlightenment.',
-    header: <Skeleton />,
-  },
-  {
-    title: 'The Joy of Creation',
-    description: 'Experience the thrill of bringing ideas to life.',
-    header: <Skeleton />,
-  },
-  {
-    title: 'The Spirit of Adventure',
-    description: 'Embark on exciting journeys and thrilling discoveries.',
-    header: <Skeleton />,
-  },
-];
